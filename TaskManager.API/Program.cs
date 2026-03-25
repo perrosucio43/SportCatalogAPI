ea545f538da389d2fcfaf9aa257c6d33efa7812b
Matias <matiasdarosa742@gmail.com>
2026-03-24 21:42:02 -0300

    enable swagger and railway port fix

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -98,15 +98,14 @@ builder.Services.AddAuthentication(options =>
 
 
 var app = builder.Build();
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://*:{port}");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
