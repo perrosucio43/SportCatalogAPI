b7b8b4d059a4ce3fc844d64148ec4b751d6fc857
Matias <matiasdarosa742@gmail.com>
2026-03-25 00:49:19 -0300

    fix jwt

--- TaskManager.Application/Services/JwtServices.cs
+++ TaskManager.Application/Services/JwtServices.cs
@@ -41,27 +41,34 @@ namespace TaskManager.Application.Services
 
        };
 
-            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_Config["Jwt:Key"]));
+            var key = Environment.GetEnvironmentVariable("JWT_KEY")
+          ?? _Config["Jwt:Key"];
 
+            if (string.IsNullOrEmpty(key))
+                throw new Exception("JWT_KEY no configurada");
 
+            var issuer = Environment.GetEnvironmentVariable("JWT_ISSUER")
+             ?? _Config["Jwt:Issuer"];
 
-            var creds = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
+            var audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE")
+                           ?? _Config["Jwt:Audience"];
+
+            var duration = Environment.GetEnvironmentVariable("JWT_DURATION")
+                           ?? _Config["Jwt:DurationInMinutes"];
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+            var creds = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(
-
-                issuer: _Config["Jwt:Issuer"],
-                audience: _Config["Jwt:Audience"],
+                issuer: issuer,
+                audience: audience,
                 claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_Config["Jwt:DurationInMinutes"])),
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(duration)),
+                signingCredentials: creds
+            );
 
-                signingCredentials: creds);
-
-
-
-
-
-            return new JwtSecurityTokenHandler().WriteToken(token);  
-
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
 
